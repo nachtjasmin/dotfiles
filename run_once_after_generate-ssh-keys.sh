27577e951b0b80842d1f3cd247bbc35cb16cf765
nachtjasmin <nachtjasmin@outlook.de>
2022-02-03 14:31:40 +0100

    fix: ask for SSH key generation

--- run_once_after_generate-ssh-keys.sh
+++ run_once_after_generate-ssh-keys.sh
@@ -5,6 +5,14 @@ if ! command -v ssh-keygen &>/dev/null; then
 fi
 
 destination="$HOME/.ssh/id_ed25519"
+if [ -f "$destination" ]; then
+	exit 0; # already created
+fi
+
+read -e -p "Generate SSH key? Type y to confirm: " choice
+if [[ ! "$choice" == [Yy]* ]]; then
+	exit 0;
+fi;
 
 # if file does not exist, generate a new SSH key
 if [ ! -f "$destination" ]; then
