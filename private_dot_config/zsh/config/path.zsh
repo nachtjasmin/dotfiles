5823d657881d887591c1c77b22f7db271174d4ca
nachtjasmin <nachtjasmin@posteo.de>
2023-05-12 15:21:02 +0200

    Use fnm as node version manager

--- private_dot_config/zsh/config/path.zsh
+++ private_dot_config/zsh/config/path.zsh
@@ -3,7 +3,3 @@ export PATH="$PATH:$HOME/go/bin"
 
 # Add Rust binaries
 export PATH="$PATH:$HOME/.cargo/bin"
-
-# Sudoless npm global packages (https://wiki.archlinux.org/index.php/Node.js#Allow_user-wide_installations)
-PATH="$HOME/.local/bin:$PATH"
-export npm_config_prefix="$HOME/.local"
