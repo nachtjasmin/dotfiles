82d098c2345bff6451156a1d4c866906ece73d6f
nachtjasmin <nachtjasmin@posteo.de>
2023-06-27 18:16:56 +0200

    Update aliases, add hx=helix

--- private_dot_config/zsh/config/aliases.zsh
+++ private_dot_config/zsh/config/aliases.zsh
@@ -1,25 +1,21 @@
 # Git
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias nah='git nah'
-alias gitroot='cd "$(git rev-parse --show-toplevel)"'
 
-# Chezmoi
+# Several tools
 alias cz=chezmoi
+alias hx=helix
 
 # Directories
 alias ..="cd .."
-
-# Docker
-alias docker-nas='docker -H tcp://192.168.178.200:2376'
 
 # Enable aliases to be sudo’ed
 alias sudo='sudo '
 
 # Default linux tools
 alias mkdir='mkdir -p'
-alias grep='rg' # ripgrep
+alias grep='rg'
 alias dd='dd status=progress'
 alias cat='bat'
 alias ls='exa'
-
 alias ip="ip --color=auto"
