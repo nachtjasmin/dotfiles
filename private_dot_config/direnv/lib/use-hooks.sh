591b62b863df4f9801ca02380a2e1773542f6bc6
nachtjasmin <nachtjasmin@posteo.de>
2023-10-18 11:07:13 +0200

    Add direnv helper for setting up Git hooks

---
+++ private_dot_config/direnv/lib/use-hooks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Simplifies the setup of common Git hook managers by just needing a
+#	use hooks
+# line inside the .envrc.
+#
+# TODO: Check whether hooks are already installed and if so, skip the setup.
+
+use_hooks() {
+	if has pre-commit && [[ -f ".pre-commit-config.yaml" ]]; then
+	  pre-commit install
+	fi
+
+	if has lefthook && [[ -f ".lefthook.yml" ]]; then
+		lefthook install
+	fi
+}
