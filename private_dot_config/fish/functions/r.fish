4289f3d354ddba0245a17d9fada426159a53f3a0
nachtjasmin <nachtjasmin@posteo.de>
2024-06-27 21:36:50 +0200

    fish: support jj repos and sort them

--- private_dot_config/fish/functions/r.fish
+++ private_dot_config/fish/functions/r.fish
@@ -1,7 +1,7 @@
 function r --description 'Switches to the given Git repository. Any arguments are passed to fzf.'
     set repo_path "$HOME/repos/"
-    set repos (fd -t directory --hidden  '\.git$' --max-depth 4 $repo_path)
+    set repos (fd -t directory --hidden  '\.(jj|git)$' --max-depth 4 $repo_path)
     set normalized (path dirname $repos | string replace $repo_path "")
-    set chosen (printf "%s\n" $normalized | fzf $argv)
+    set chosen (printf "%s\n" $normalized | sort --unique | fzf $argv)
     cd "$repo_path$chosen"
 end
