831b14f189ea2efe31675ad9119b59c3d4ab9a97
Jasmin Oster <joster@anexia.com>
2024-08-22 13:29:44 +0200

    fish(r): only switch if path was chosen

--- private_dot_config/fish/functions/r.fish
+++ private_dot_config/fish/functions/r.fish
@@ -3,5 +3,5 @@ function r --description 'Switches to the given Git repository. Any arguments ar
     set repos (fd -t directory --hidden  '\.(jj|git)$' --max-depth 4 $repo_path)
     set normalized (path dirname $repos | string replace $repo_path "")
     set chosen (printf "%s\n" $normalized | sort --unique | fzf $argv)
-    cd "$repo_path$chosen"
+    and cd "$repo_path$chosen"
 end
