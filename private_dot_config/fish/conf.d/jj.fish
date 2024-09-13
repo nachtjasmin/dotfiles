4e73d87e65d69efe252480a8978f918c503bcdc6
nachtjasmin <nachtjasmin@posteo.de>
2024-09-13 09:55:59 +0200

    jj: add alt+r binding to cd to repository root

--- private_dot_config/fish/conf.d/jj.fish
+++ private_dot_config/fish/conf.d/jj.fish
@@ -1,1 +1,5 @@
 jj util completion fish | source
+
+# Bind Alt+R to go to the root of the repository. We have to repaint the commandline afterwards, in
+# order to ensure that the new directory is displayed as intended.
+bind \er 'cd (jj root); commandline -f repaint'
