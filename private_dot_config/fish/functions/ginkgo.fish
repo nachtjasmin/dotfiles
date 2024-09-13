70d0186a683d9f81baeaf27f9895b720f3c828d4
nachtjasmin <nachtjasmin@posteo.de>
2024-09-13 09:55:59 +0200

    ginkgo: add wrapper around "go run"

---
+++ private_dot_config/fish/functions/ginkgo.fish
@@ -0,0 +1,3 @@
+function ginkgo --wraps='go run github.com/onsi/ginkgo/v2/ginkgo' --description 'alias ginkgo=go run github.com/onsi/ginkgo/v2/ginkgo'
+    go run github.com/onsi/ginkgo/v2/ginkgo $argv
+end
