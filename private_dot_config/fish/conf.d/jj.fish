7bed24eaa28efdc70bc353d58928dbf8dfa8009b
nachtjasmin <nachtjasmin@posteo.de>
2024-11-04 20:16:54 +0100

    fish: Add jj bindings only if it's present

--- private_dot_config/fish/conf.d/jj.fish
+++ private_dot_config/fish/conf.d/jj.fish
@@ -1,5 +1,7 @@
-jj util completion fish | source
+command -q jj; and begin
+    jj util completion fish | source
 
-# Bind Alt+R to go to the root of the repository. We have to repaint the commandline afterwards, in
-# order to ensure that the new directory is displayed as intended.
-bind \er 'cd (jj root); commandline -f repaint'
+    # Bind Alt+R to go to the root of the repository. We have to repaint the commandline afterwards, in
+    # order to ensure that the new directory is displayed as intended.
+    bind \er 'cd (jj root); commandline -f repaint'
+end
