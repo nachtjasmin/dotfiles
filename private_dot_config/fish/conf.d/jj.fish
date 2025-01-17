c449dce8261dfd1f699d41b196a48acadce56a84
nachtjasmin <nachtjasmin@posteo.de>
2025-01-17 11:47:08 +0100

    fish: use dynamic completions for jj

--- private_dot_config/fish/conf.d/jj.fish
+++ private_dot_config/fish/conf.d/jj.fish
@@ -1,5 +1,10 @@
 command -q jj; and begin
-    jj util completion fish | source
+    # There are static and dynamic completions in jj. The "jj util completion fish" command actually
+    # only prints the static completions, but not the dynamic ones, added in v0.24.0.
+    #
+    # Note: This does not seem to work reliably on Fish 4.0 (currently in beta).
+    # See also: https://github.com/fish-shell/fish-shell/pull/11046
+    COMPLETE=fish jj | source
 
     # Bind Alt+R to go to the root of the repository. We have to repaint the commandline afterwards, in
     # order to ensure that the new directory is displayed as intended.
