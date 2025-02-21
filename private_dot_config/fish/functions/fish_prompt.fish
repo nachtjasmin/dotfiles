b4a4040ff5c06efde65cc9b181b0e96375c69b7c
nachtjasmin <nachtjasmin@posteo.de>
2025-02-21 12:13:41 +0100

    fish: Display namespace and cluster name for k8s

--- private_dot_config/fish/functions/fish_prompt.fish
+++ private_dot_config/fish/functions/fish_prompt.fish
@@ -34,10 +34,26 @@ function fish_prompt
 
     # Check current kubernetes context
     command -q kubectl
-    and set -l _kubectx (kubectl config current-context 2> /dev/null)
+    and begin
+        set -l _current_context (kubectl config view --minify -ojson | jq '.contexts[0].context')
+        set -l _current_cluster (echo $_current_context | jq -r '.cluster')
 
-    if test -n "$_kubectx"
-        set kube_context (set_color blue)" on $_kubectx$normal"
+        # Each cluster is managed via a different kubeconfig file (see kctx.fish).
+        # If it's set, take the filename (without extension) as an additional description.
+        if test -n "$KUBECONFIG"
+            set _current_cluster "$(string replace -r '\.ya?ml' '' (path basename $KUBECONFIG)) ($_current_cluster)"
+        end
+
+        # If not known, the namespace is "default".
+        set -l _current_namespace (echo $_current_context | jq -r '.namespace // "default"')
+        set -f _kubectx $_current_cluster
+
+        # If the namespace is available, set it as well.
+        if test -n "$_current_namespace"
+            set -fa _kubectx in $_current_namespace
+        end
+
+        set -f kube_context (set_color blue)" on $_kubectx$normal"
     end
 
     # Only show host if in SSH or container
