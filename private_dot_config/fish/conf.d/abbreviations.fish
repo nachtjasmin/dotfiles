f53937eab941084d77f8811931aa260191d77520
nachtjasmin <nachtjasmin@posteo.de>
2025-05-24 14:49:41 +0200

    fish: add kubectl abbreviations

--- private_dot_config/fish/conf.d/abbreviations.fish
+++ private_dot_config/fish/conf.d/abbreviations.fish
@@ -2,3 +2,7 @@
 
 # Allows me to write "..." for an automatic "cd ../../".
 abbr --add dotdot --regex '^\.\.+$' --function multicd
+
+# Kubernetes-related stuff
+abbr --add k kubectl
+abbr --add kk kubectl klock
